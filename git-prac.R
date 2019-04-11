60a39a6b07c76711eff537cb3e46b4fed16a32b2
JoelWoon <j.woon@liverpool.ac.uk>
2019-04-11 09:53:27 +0100

    add line

--- git-prac.R
+++ git-prac.R
@@ -1,4 +1,6 @@
 ## Git practise script
 
+
+usethis::edit_r_environ()
 usethis::use_git_config(user.name = "JoelWoon", user.email = "j.woon@liverpool.ac.uk")
 usethis::use_git_protocol(protocol =  "https")
