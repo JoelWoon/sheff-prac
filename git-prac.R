6073f6735e19ac17a7a18aac572f193166c8aa7c
JoelWoon <j.woon@liverpool.ac.uk>
2019-04-11 09:55:26 +0100

    change to commit

--- git-prac.R
+++ git-prac.R
@@ -3,4 +3,4 @@
 
 usethis::edit_r_environ()
 usethis::use_git_config(user.name = "JoelWoon", user.email = "j.woon@liverpool.ac.uk")
-usethis::use_git_protocol(protocol =  "https")
+usethis::use_github(protocol =  "https")
